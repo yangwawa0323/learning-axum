57f97da461b8b24eb26291669e79b5f6f39abdd6
yangwawa0323 <yangwawa0323@163.com>
2024-08-11 00:39:01 +0800

    IntoResponse

--- src/users/interactive.rs
+++ src/users/interactive.rs
@@ -1,7 +1,15 @@
-use axum::{routing::get, Router};
+use axum::{
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
 
 pub fn create_route() -> Router {
     let user_app = Router::new()
+        .route("/hi_resp_obj", get(say_hi_vec_response_obj))
+        .route("/hi_resp", get(say_hi_vec_response))
+        .route("/hi3", get(say_hi_vec))
+        .route("/hi2", get(say_hi2))
         .route("/hi", get(say_hi))
         .route("/welcome", get(welcome).post(welcome));
     user_app
@@ -13,4 +21,23 @@ async fn welcome() -> String {
 
 async fn say_hi() -> String {
     "Hi!".to_string()
+}
+
+async fn say_hi2() -> &'static [u8] {
+    "Hello".as_bytes()
+}
+
+async fn say_hi_vec() -> Vec<u8> {
+    let resp = "Hello 5117xue.com".as_bytes().to_vec();
+    resp
+}
+
+async fn say_hi_vec_response() -> impl IntoResponse {
+    let resp = "Hello 51cloudclass.com".as_bytes().to_vec();
+    resp
+}
+
+async fn say_hi_vec_response_obj() -> Response {
+    let resp = "Hello 51cloudclass.com 5117xue.com".as_bytes().to_vec();
+    resp.into_response()
 }
