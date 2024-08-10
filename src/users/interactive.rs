6f1f9fc3802fbced52449801ffad9bb8d8c1843f
yangwawa0323 <yangwawa0323@163.com>
2024-08-11 01:10:48 +0800

    Body extractor

--- src/users/interactive.rs
+++ src/users/interactive.rs
@@ -1,11 +1,14 @@
 use axum::{
+    body::Body,
     response::{IntoResponse, Response},
-    routing::get,
+    routing::{get, post},
     Router,
 };
+use http_body_util::BodyExt;
 
 pub fn create_route() -> Router {
     let user_app = Router::new()
+        .route("/uppercase", post(echo_uppercase))
         .route("/hi_resp_obj", get(say_hi_vec_response_obj))
         .route("/hi_resp", get(say_hi_vec_response))
         .route("/hi3", get(say_hi_vec))
@@ -40,4 +43,16 @@ async fn say_hi_vec_response() -> impl IntoResponse {
 async fn say_hi_vec_response_obj() -> Response {
     let resp = "Hello 51cloudclass.com 5117xue.com".as_bytes().to_vec();
     resp.into_response()
+}
+
+async fn echo_uppercase(req: Body) -> impl IntoResponse {
+    let frame = req.map_frame(|frame| {
+        frame.map_data(|data| {
+            data.iter()
+                .map(|byte| byte.to_ascii_uppercase())
+                .collect::<axum::body::Bytes>()
+        })
+    });
+    let resp = Response::new(frame);
+    resp
 }
