7dafdaf6dffff613edced60b5c5fd7bb6faa5286
yangwawa0323 <yangwawa0323@163.com>
2024-08-18 02:03:15 +0800

    State vs. middleware

--- src/users/extension.rs
+++ src/users/extension.rs
@@ -1,7 +1,7 @@
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use axum::{
-    extract::Request,
+    extract::{Request, State},
     http::StatusCode,
     middleware::{from_fn, Next},
     response::Response,
@@ -33,6 +33,10 @@ pub fn create_routes() -> Router {
         .route("/shared-data-2", get(shared_data_2))
         .layer(Extension(db_data))
         .route_layer(from_fn(custom_middleware))
+        .route("/with-counter-state", get(with_counter_state))
+        .with_state(CounterState {
+            count: Arc::new(Mutex::new(0)),
+        })
 }
 
 async fn shared_data_1(Extension(db): Extension<Arc<AppState>>) -> String {
@@ -48,4 +52,15 @@ async fn custom_middleware(req: Request, next: Next) -> Result<Response, StatusC
         return Err(StatusCode::BAD_REQUEST);
     }
     Ok(next.run(req).await)
+}
+
+#[derive(Clone, Debug)]
+pub struct CounterState {
+    pub count: Arc<Mutex<i32>>,
+}
+
+async fn with_counter_state(State(counter): State<CounterState>) -> String {
+    let mut count = counter.count.lock().unwrap();
+    *count += 1;
+    format!("counter : {}", count)
 }
